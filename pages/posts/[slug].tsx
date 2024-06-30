f6a4c90439bb41152d866358608f29de4c60f4ec
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2024-06-30 16:07:45 +0700

    Fix img meta tag resulation

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -58,7 +58,7 @@ export default function ProjectSlugRoute(props: PageProps) {
     fetchPostsByCategory(post.category.name, setCategoriesWithPosts)
   }, [post.category.name])
 
-  const postImage = urlForImage(post.coverImage).url()
+  const postImage = urlForImage(post.coverImage).height(1500).width(1000).url()
 
   console.log('img', postImage)
 
