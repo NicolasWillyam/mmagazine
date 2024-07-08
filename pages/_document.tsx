0e0ff87ca15129f3df5d4d3319b77585fa5aa900
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2024-07-08 22:40:42 +0700

    Fix mobile favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/logo-black.svg" />
+        {/* Add your favicon link here */}
+        <link rel="shortcut icon" href="/logo-black.svg" />
+        {/* Add your mobile favicon link here */}
+        <link rel="apple-touch-icon" href="/logo-black.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <title>M MAGAZINE Vietnam</title>
         <meta name="description" content="M MAGAZINE Vietnam" />
         <meta property="og:image" content="/logo-black.svg" />
