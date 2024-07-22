9f6feb2eabec483508caba68d895a1737ac84e13
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2024-07-22 23:44:11 +0700

    Test Meta Tags

--- components/PostPageHead.tsx
+++ components/PostPageHead.tsx
@@ -30,9 +30,25 @@ export default function PostPageHead({ post }: PostPageHeadProps) {
         content={`https://www.mmagazinevietnam.com/posts/${post.slug}`}
       />
       <meta property="og:type" content="article" />
-      <meta property="og:title" content={post.title} />
-      <meta property="og:description" content={post?.description || ''} />
-      <meta property="og:image" content={postImage} />
+      <meta
+        property="og:title"
+        content={
+          'Matthieu Blazy chia sẻ về việc học hỏi từ Le Corbusier và sự hợp tác mới của ông với Bottega Veneta trong lĩnh vực đồ nội thất'
+        }
+      />
+      <meta
+        property="og:description"
+        content={
+          post?.description ||
+          'Matthieu Blazy chia sẻ về việc học hỏi từ Le Corbusier và sự hợp tác mới của ông với Bottega Veneta trong lĩnh vực đồ nội thất'
+        }
+      />
+      <meta
+        property="og:image"
+        content={
+          'https://cdn.sanity.io/images/y9go1tpw/production/7c02c3e29623c82be612356283b8137a0e8822dd-680x1020.png?fit=max&auto=format'
+        }
+      />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="mmagazinevietnam.com" />
